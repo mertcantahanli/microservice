3b8b8b7e9067150ab12b63eb834258213d02cf67
MERT CAN TAHANLI <45268111+mertcantahanli@users.noreply.github.com>
2023-05-10 19:01:08 +0300

    first commit

---
+++ filter-service/src/test/java/com/kodlamaio/filterservice/FilterServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kodlamaio.filterservice;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
